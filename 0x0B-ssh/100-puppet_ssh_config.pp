b2135fa505e9d3ae1043da6de3e1dc733abf9d77
knottedbrain <yilkalargawworkneh@gmail.com>
2023-09-25 01:22:56 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # set up a client SSH configuration
 
-include stdlib
-
 file_line { 'Configure identityfile':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school'
-  ensure => present,
+
 }
 
 file_line { 'Configure password authentication to no':
+  ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   ensure => present,
