c6767fddbea40458eb24e903a219ca9accf7748d
knottedbrain <yilkalargawworkneh@gmail.com>
2023-09-25 01:24:00 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,12 +4,10 @@ file_line { 'Configure identityfile':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school'
-
 }
 
 file_line { 'Configure password authentication to no':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  ensure => present,
 }
