f6e78c9079215267534b12a6001b18a3e6875801
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-27 05:30:35 +0300

    attempting 0x0C-web_server/7-puppet_install_nginx_web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# nginx puppet
+
+package { 'nginx':
+  ensure => present,
+  name   => 'nginx',
+}
+
+
+file { 'index.html':
+    ensure => present,
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+    }
+
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me'],
+}
