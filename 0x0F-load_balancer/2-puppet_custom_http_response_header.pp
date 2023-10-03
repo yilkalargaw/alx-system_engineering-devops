3f0f974be36fa2ea8dcbc7c13a2d8d2e13e2dd42
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-10-03 02:19:18 +0000

    attempting 0x0F-load_balancer/2-puppet_custom_http_response_header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# nginx puppet
+
+package { 'nginx':
+  ensure => present,
+  name   => 'nginx',
+}
+
+
+file { 'index.html':
+    ensure => present,
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+    }
+
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'addHeader':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure     => running,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me'],
+}
